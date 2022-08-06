2833e03c11e989111e60eff07b608a302ccf8303
Karpov Yuri <yurick@bk.ru>
2022-08-06 14:05:49 +0300

    feat: Добавить контроллер users

---
+++ controllers/users.js
@@ -0,0 +1,74 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const NotFoundError = require('../errors/not-found-err');
+const BadRequestError = require('../errors/bad-request-err');
+const ConflictError = require('../errors/conflict-err');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// Получить данные о пользователе
+const getUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(() => { throw new NotFoundError('Пользователь не найден'); })
+    .then((user) => res.send(user))
+    .catch(next);
+};
+
+// Создание пользоателя
+const createUser = (req, res, next) => {
+  const { name, email, password } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({ name, email, password: hash })
+        .then((user) => {
+          const returnUser = user.toObject();
+          delete returnUser.password;
+          res.send(returnUser);
+        })
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
+          } else if (err.code === 11000) {
+            next(new ConflictError('Пользователем с такими e-mail уже существует'));
+          } else next(err);
+        });
+    });
+};
+
+// Обновление пользователя
+const updateUser = (req, res, next) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .orFail(() => { throw new NotFoundError('Пользователь с указанным _id не найден'); })
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при обновлении профиля'));
+      } else next(err);
+    });
+};
+
+// Авторизация пользователя
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      res.cookie('jwt', token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: true,
+      });
+      res.status(200).send({ _id: user._id, message: 'Авторизация прошла успешно' });
+    })
+    .catch(next);
+};
+
+// Экспорт модулей
+module.exports = {
+  getUser,
+  createUser,
+  updateUser,
+  login,
+};
